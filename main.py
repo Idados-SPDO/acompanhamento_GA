d9e61e4e179c530b7c073ee11156760d0fe8e1d7
FGV Idados_SPDO <idados_spdo@fgv.br>
2024-05-29 14:09:52 -0300

    Update main.py dt.time

--- main.py
+++ main.py
@@ -54,8 +54,12 @@ def main():
         construcao_atualizada['Status da Demanda'] = construcao_atualizada.apply(lambda row: 'Finalizada' if row['Status do GA'] in ['Concluída', 'Cancelada'] else row['Status da Demanda'], axis=1)
         industria_atualizada['Status da Demanda'] = industria_atualizada.apply(lambda row: 'Finalizada' if row['Status do GA'] in ['Concluída', 'Cancelada'] else row['Status da Demanda'], axis=1)
 
-        construcao_atualizada['Data Retorno'] = construcao_atualizada['Data Retorno'].dt.date
-        industria_atualizada['Data Retorno'] = industria_atualizada['Data Retorno'].dt.date
+        if not construcao_atualizada['Data Retorno'].isnull().all():
+        construcao_atualizada['Data Retorno'] = pd.to_datetime(construcao_atualizada['Data Retorno']).dt.date
+
+        if not industria_atualizada['Data Retorno'].isnull().all():
+        industria_atualizada['Data Retorno'] = pd.to_datetime(industria_atualizada['Data Retorno']).dt.date
+
 
         output = BytesIO()
         with pd.ExcelWriter(output, engine='openpyxl') as writer:
